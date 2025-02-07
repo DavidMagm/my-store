c1ea0d30273f56f0105c5969cf3159e094cd4987
david torres <david.martinezezguerra@gmail.com>
2025-02-06 19:06:06 -0500

    config

--- config/config.js
+++ config/config.js
@@ -1,11 +1,16 @@
 const {config} = require('../config');
 
-const URI = `postgres://${config.dbUser}:${config.dbPassword}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 module.exports = {
   development: {
-    url: URI,
+    url: config.dbUrl,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+          require: true,
+          rejectUnauthorized: false,
+      }
+    }
   },
   production: {
     url: config.dbUrl,
