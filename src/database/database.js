8220c1a08d83362d28704cae99f501d0b67fa6d8
david torres <david.martinezezguerra@gmail.com>
2025-02-10 23:15:00 -0500

    options

--- src/database/database.js
+++ src/database/database.js
@@ -2,15 +2,19 @@ const {Sequelize} = require('sequelize');
 const setupModels = require('../../models');
 const {config} = require('../../config');
 
-const sequelize = new Sequelize(config.dbUrl, {
-    dialect: 'postgres',
-    dialectOptions: {
+const options = {
+    dialect: 'postgres'
+}
+
+if(config.isProd) {
+    options.dialectOptions = {
         ssl: {
             require: true,
             rejectUnauthorized: false,
         }
     }
-});
+}
+const sequelize = new Sequelize(config.dbUrl, options);
 
 setupModels(sequelize);
 
