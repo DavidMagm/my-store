1c24f01bc0636c58aaa852d4fe945e97a02172c5
david torres <david.martinezezguerra@gmail.com>
2025-02-10 23:22:52 -0500

    new deploy

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const routerApi = require('./routes');
 const { logError, boomErrorHandler, sqlErrorHandler, errorHandler } = require('./middleware/errorHandler');
-const port = 3000;
+const port = process.env.port || 3000;
 app.use(express.json());
 
 const ipList = ['http://localhost:5500', 'http://127.0.0.1:5500'];
